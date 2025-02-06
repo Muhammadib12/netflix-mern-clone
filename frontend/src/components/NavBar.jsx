1534d5f3c7f5761b75b853841d756dd936d77c76
unknown <mohamadibra403@gmail.com>
2025-02-06 18:16:56 +0200

    Updated NavBar, AuthScreen components and replaced ultra.webp with ultra.png

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -22,7 +22,7 @@ function NavBar() {
     <header className="max-w-6xl relative mx-auto flex flex-wrap items-center justify-between p-4 h-20">
         <div className="flex items-center gap-10 z-50 ">
             <Link to={"/"}>
-                <img src='/ultraflix.jpg' alt='netflixlogo' className='w-32 sm:w-40 bg-transparent border border-none' />
+            <img src='/ultra.png' alt='netflixlogo' className='w-32 sm:w-40  bg-transparent border-0 block' />
             </Link>
 
             {/* Desktop navbar items */}
--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -15,7 +15,12 @@ function AuthScreen() {
     {/* Nav Bar */}
     <header className="max-w-6xl mx-auto flex items-center justify-between p-4 pb-10">
 
-    <img src="/netflix-logo.png" alt="Netflix-logo" className="w-32 md:w-52"/>
+    <img 
+  src="/ultra.png" 
+  alt="UltraFlix logo" 
+  className="w-32 sm:w-40 bg-transparent border-none block"
+  style={{ backgroundColor: "transparent" }}
+/>
     <Link to={"/login"} className="text-white bg-red-600 py-1 px-2 rounded">Login</Link>
 
     </header>
